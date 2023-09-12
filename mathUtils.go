2ef23a74150e489573e81d2c50afc9a3703a56ae
luigi vanacore <luigi.vanacore@outlook.com>
2023-09-12 23:56:53 +0200

    added mathUtils

---
+++ mathUtils.go
@@ -0,0 +1,42 @@
+package ebiten_extended
+
+import "math"
+
+//function and constat used for Math 2D
+
+const FLOAT_PI float64 = 3.14159265358979323846
+
+func Min(x float64, y float64) float64 {
+	if x == 0 || x == y {
+		return x
+	}
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max(x float64, y float64) float64 {
+	if x == 0 || x == y {
+		return x
+	}
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func Distance(x, y, x2, y2 float64) float64 {
+	dx := x - x2
+	dy := y - y2
+	ds := (dx * dx) + (dy * dy)
+	return math.Sqrt(math.Abs(ds))
+}
+
+func DegreesToRadian(degree float64) float64 {
+	return degree * FLOAT_PI / 180.0
+}
+
+func RadianToDegrees(radian float64) float64 {
+	return radian * 180.0 / FLOAT_PI
+}
