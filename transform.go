23a78ad860f5fb44603bf828e1fc5e9bada10665
luigi vanacore <luigi.vanacore@outlook.com>
2023-09-13 15:01:21 +0200

    added transform

---
+++ transform.go
@@ -0,0 +1,55 @@
+package ebiten_extended
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Transform struct {
+	position Vector2D
+	pivot    Vector2D
+	rotation int
+	geoM     ebiten.GeoM
+}
+
+func (t *Transform) GetPosition() Vector2D {
+	return t.position
+}
+
+func (t *Transform) SetPosition(x, y float64) {
+	t.position.X = x
+	t.position.Y = y
+}
+
+func (t *Transform) GetRotation() int {
+	return t.rotation
+}
+
+func (t *Transform) SetRotation(rotation int) {
+	t.rotation = rotation
+}
+
+func (t *Transform) SetPivot(x, y float64) {
+	t.pivot.X = x
+	t.pivot.Y = y
+}
+
+func (t *Transform) GetPivot() Vector2D {
+	return t.pivot
+}
+
+func (t *Transform) GetGeoM() ebiten.GeoM {
+	return t.geoM
+}
+
+func (t *Transform) SetGeoM(geoM ebiten.GeoM) {
+	t.geoM = geoM
+}
+
+func (t *Transform) Rotate(rotation int) {
+	t.rotation += rotation
+}
+
+func (t *Transform) Move(x, y float64) {
+	t.position.X += x
+	t.position.Y += y
+}
