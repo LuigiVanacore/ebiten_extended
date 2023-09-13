2c5b95a136f3b3b50352b0513232ef6d975f1a17
luigi vanacore <luigi.vanacore@outlook.com>
2023-09-13 15:01:08 +0200

    added bitvector

---
+++ bitvector.go
@@ -0,0 +1,29 @@
+package ebiten_extended
+
+import "errors"
+
+type Bitvector []byte
+
+func IsBitSet(bitvector []byte, n int) (bool, error) { // returns true if bit n is set; false otherwise.
+	if n > len(bitvector) {
+		return false, errors.New("n is greater than the size of bitvector")
+	}
+	return bitvector[n/8]&(1<<(n%8)) > 0, nil
+}
+
+func Bitset(bitvector []byte, n int) error {
+	if n > len(bitvector) {
+		return errors.New("n is greater than the size of bitvector")
+	}
+	bitvector[n/8] |= (1 << (n % 8)) // |= is 'bitwise or equals', analogous to +=
+	return nil
+}
+
+func Bitunset(bitvector []byte, n int) error {
+	if n > len(bitvector) {
+		return errors.New("n is greater than the size of bitvector")
+	}
+	bitvector[n/8] -= (1 << (n % 8))
+
+	return nil
+}
