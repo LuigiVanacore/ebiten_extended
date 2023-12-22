bfee162f21078f0b13bea25360a2bad9d7c9b0f5
Luigi Vanacore <luigi.vanacore@outlook.com>
2023-12-22 22:12:11 +0700

    add layer struct

---
+++ layer.go
@@ -0,0 +1,39 @@
+package ebiten_extended
+
+
+
+type Layer struct {
+	id int
+	priority int
+	rootScene *SceneNode
+}
+
+func NewLayer(id int, priority int) *Layer {
+	return &Layer{ id: id, priority: priority, rootScene: NewRootSceneNode()}
+}
+
+
+func (l *Layer) GetId() int {
+	return l.id
+}
+
+func (l *Layer) SetId(id int) {
+	l.id = id
+}
+
+func (l *Layer) SetPriority(priority int) {
+	l.priority = priority
+}
+
+func (l *Layer) GetPriority() int {
+	return l.priority
+}
+
+func (l *Layer) AddNode( node *SceneNode) {
+	l.rootScene.Addhildren(node)
+}
+
+func (l *Layer) GetRootScene() *SceneNode {
+	return l.rootScene
+}
+
