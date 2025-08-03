119edd3902ef5bfbb826fa8dcd78a768dbf2edb4
luigi.vanacore <luigi.vanacore@outlook.com>
2025-08-03 23:36:44 +0200

    added animationSprite

--- animationSet.go
+++ animationSet.go
@@ -34,6 +34,60 @@ func (a *AnimationSet) GetTexture() *ebiten.Image {
 	return a.spriteSheet[a.currentFrame]
 }
 
+func (a *AnimationSet) GetPivot() math2D.Vector2D {
+	return a.pivot
+}
+
+func (a *AnimationSet) SetPivot(pivot math2D.Vector2D) {
+	a.pivot = pivot
+}
+
+func (a *AnimationSet) GetFrameCount() uint {
+	return a.frameCount
+}
+
+func (a *AnimationSet) SetFrameCount(frameCount uint) {
+	a.frameCount = frameCount
+	if a.currentFrame >= a.frameCount {
+		a.currentFrame = 0
+	}
+}
+
+func (a *AnimationSet) GetCurrentFrame() uint {
+	return a.currentFrame
+}
+
+func (a *AnimationSet) SetCurrentFrame(frame uint) {
+	if frame < a.frameCount {
+		a.currentFrame = frame
+	} else {
+		a.currentFrame = 0
+	}
+}
+
+func (a *AnimationSet) GetTimePerFrame() float64 {
+	return a.timePerFrame
+}
+
+func (a *AnimationSet) SetTimePerFrame(timePerFrame float64) {
+	if timePerFrame < 0 {
+		timePerFrame = 1
+	}
+	
+	a.timePerFrame = timePerFrame
+	
+}
+
+func (a *AnimationSet) SetLooped(isLooped bool) {
+	a.isLooped = isLooped
+}
+
+func (a *AnimationSet) IsLooped() bool {
+	return a.isLooped
+}
+
+
+
 func (a *AnimationSet) Update() {
 
 	if a.IsEnded() && !a.isLooped {
---
+++ animationSprite.go
@@ -0,0 +1,45 @@
+package ebiten_extended
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type AnimationSprite struct {
+	Node2D
+	animationSet *AnimationSet
+	layer        int
+	isPlaying    bool
+}
+
+func NewAnimationSprite(name string, animationSet *AnimationSet, layer int, isPlaying bool) *AnimationSprite {
+	return &AnimationSprite{
+		Node2D:       *NewNode2D(name),
+		animationSet: animationSet,
+		layer:        layer,
+		isPlaying:    isPlaying,
+	}
+}
+
+func (a *AnimationSprite) Start() {
+	a.isPlaying = true
+}
+
+func (a *AnimationSprite) Stop() {
+	a.isPlaying = false
+}
+
+func (a *AnimationSprite) GetLayer() int {
+	return a.layer
+}
+
+func (a *AnimationSprite) Update() {
+	if a.isPlaying {
+		if a.animationSet != nil {
+			a.animationSet.Update()
+		}
+	}
+}
+
+func (a *AnimationSprite) Draw(target *ebiten.Image, op *ebiten.DrawImageOptions) {
+	if a.animationSet != nil {
+		a.animationSet.Draw(target, op)
+	}
+}
