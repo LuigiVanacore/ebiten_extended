c314983f5088c95ea9c3e6f3b18a71267dfdc852
luigi.vanacore <luigi.vanacore@outlook.com>
2025-08-03 23:38:52 +0200

    modified stack

--- utils/stack.go
+++ utils/stack.go
@@ -18,16 +18,15 @@ func (s *Stack[T]) Push(value T) {
 // Pop removes and returns the top element from the stack
 // Returns the zero value of T and false if the stack is empty
 func (s *Stack[T]) Pop() (T, bool) {
-
     if len(s.data) == 0 {
         var zero T
-        return zero, false 
+        return zero, false
     }
-    // Get the last element
-    top := s.data[len(s.data)-1]
-    // Remove the last element
-    s.data = s.data[:len(s.data)-1]
-    return top, true
+    // Get the first element
+    first := s.data[0]
+    // Remove the first element
+    s.data = s.data[1:]
+    return first, true
 }
 
 // Peek returns the top element without removing it
